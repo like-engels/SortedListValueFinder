cd0a5266bed64ac0cbf4962b6a0dd3d879ab34e6
Engels Tercero <higashikataengels@icloud.com>
2023-03-11 20:59:19 -0600

    first commit

---
+++ bin/ordered_list_value_checker.dart
@@ -0,0 +1,5 @@
+import 'package:ordered_list_value_checker/ordered_list_value_checker.dart' as ordered_list_value_checker;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ordered_list_value_checker.calculate()}!');
+}
---
+++ test/ordered_list_value_checker_test.dart
@@ -0,0 +1,8 @@
+import 'package:ordered_list_value_checker/ordered_list_value_checker.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
